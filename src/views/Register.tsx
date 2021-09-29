b6a8db6805477674a1b22de1492484556563ab49
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-09-29 16:11:46 +0530

    u register

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -10,4 +10,42 @@ type Fiedl = "name" | "email" | "password"
 
 const Signup: React.FC = () => {
     const nameRef = useRef<string>("")
+    const emailRef = useRef<string>("")
+    const passwordRef = useRef<string>("")
+    const loggedIn = useStore(useCallback(state =>state.loggedIn, []))
+    const history = usehistory()
+    useWillMount(() => {
+        if(loggedIn){
+            history.replace("/")
+        }
+    }, loggedIn)
+
+    const register = async(e: MouseEvent) => {
+        e.preventDefault()
+        try{
+            if(registerValidation()){
+                const response = await axios.post('api/users/register',
+                JSON.stringify({
+                    name: nameRef.current,
+                    email: emailRef.current,
+                    password: passwordRef.current
+                }),{
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                console.log(response.data)
+            }
+            else {
+                alert("fill appropriately")
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    const registerValidation = useCallback(() => {
+        return(regexMatch("name", nameRef.current) && regexMatch("email", emailRef.current) && regexMatch("password", passwordRef.current))
+    }, [])
 }
